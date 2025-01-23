033a573c351bfddd2ab62549e37364d0ed454f73
shbhuiyan <shbhuiyan997@gmail.com>
2025-01-23 19:15:45 +0600

    added footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className='container mx-auto mt-20'>
-            footer
+  return (
+    <footer className="bg-base-300">
+        <div className="footer container mx-auto mt-10 mb-4 footer-center text-base-content p-4">
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Sakhawat Hossen
+        </p>
         </div>
-    );
+    </footer>
+  );
 };
 
 export default Footer;
